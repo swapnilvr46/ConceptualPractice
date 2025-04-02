0197457b67b5b7519d17e3d80c8a87387ecca94b
swapnil.a.suraj <swapnil.zgr@gmail.com>
2025-04-02 06:14:11 -0600

    Add Variable

--- src/Animal.java
+++ src/Animal.java
@@ -4,51 +4,57 @@ public class Animal {
     String age;
     String colour;
     String sound;
+    String name;
 
-    Animal(String spices,String type, String age,String colour,String sound){
+    Animal(String spices, String type, String age, String colour, String sound) {
         this.spices = spices;
         this.type = type;
         this.age = age;
         this.colour = colour;
         this.sound = sound;
     }
-    void animalEat(){
+
+    void animalEat() {
 
         System.out.println("Aniaml : Having Food");
     }
-    void animalSleep(){
+
+    void animalSleep() {
 
         System.out.println("Aniaml : Sleeping");
     }
-    void animalAwake(){
+
+    void animalAwake() {
 
         System.out.println("Animal : Just Woke Up");
     }
-    void animalWalk(){
+
+    void animalWalk() {
 
         System.out.println("Aniaml : Just Walking");
     }
-    void animalFly(){
+
+    void animalFly() {
 
         System.out.println("Animal : Flying");
     }
-    void aniamlDetails(){
-        System.out.println("Which Spices: "+spices);
-        System.out.println("Animal Type : "+type);
-        System.out.println("How Old is : "+age);
-        System.out.println("Animal Color: "+colour);
-        System.out.println("What Sound Produce: "+sound);
+
+    void aniamlDetails() {
+        System.out.println("Which Spices: " + spices);
+        System.out.println("Animal Type : " + type);
+        System.out.println("How Old is : " + age);
+        System.out.println("Animal Color: " + colour);
+        System.out.println("What Sound Produce: " + sound);
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to the pet shop");
-        Animal cat = new Animal("Cat","Persian","2 months","brown","Meow");
-        Animal dog = new Animal("Dog","Husky","1 month","black","Bow-Bow");
-        Animal bird = new Animal("Bird","Parrot","15 days","Green","Whisle");
+        Animal cat = new Animal("Cat", "Persian", "2 months", "brown", "Meow");
+        Animal dog = new Animal("Dog", "Husky", "1 month", "black", "Bow-Bow");
+        Animal bird = new Animal("Bird", "Parrot", "15 days", "Green", "Whisle");
         cat.animalEat();
         cat.aniamlDetails();
         dog.animalSleep();
         bird.animalFly();
     }
-
 }
